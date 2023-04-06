8d8f53da5d810a004d0e78785dfa38f37a543508
merenqow <merenkov_r@gosreestr.kz>
2023-04-06 11:27:33 +0600

    vita

--- App.tsx
+++ App.tsx
@@ -1,117 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, View, Image, ImageBackground, ActivityIndicator, Text} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <ImageBackground source={require('./Graphics/poloillustrations03.png')}
+                       resizeMode='contain'
+                       style={{transform: [{ scale: 0.375 }], flex: 2}}>
+      </ImageBackground>
+      <View style={{flex: 2}}>
+      <ImageBackground source={require('./Graphics/poloillustrations01.png')}
+                       resizeMode='contain'
+                       style={{transform: [{ scale: 0.775 }], flex: 2}}>
+         <ImageBackground source={require('./Graphics/poloillustrations04.png')}
+                          resizeMode='contain'
+                          style={{transform: [{ scale: 1 }], flex: 2}}>
+                  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                       <Image
+                        source={require('./Graphics/poloillustrations05.png')}
+                        resizeMode='contain'
+                        style={{transform: [{ scale: 0.4 }]}}
+                        />
+                  </View>
+          </ImageBackground>
+      </ImageBackground>
+      </View>
+    <View style={{flex: 2, alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#CC3333" style={{transform: [{ scale: 1.5 }]}}/>
+        <Text style={{top: 20, fontFamily: 'Mulish-Semibold'}}>Загрузка</Text>
+    </View>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
   },
 });
 
